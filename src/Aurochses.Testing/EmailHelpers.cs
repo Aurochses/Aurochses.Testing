442da84c5f91ffec6a735712870e667050145233
Vladimir Rodchenko <vladimir@aurochses.com>
2017-06-25 22:01:42 +0300

    initial code

---
+++ src/Aurochses.Testing/EmailHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aurochses.Testing
+{
+    /// <summary>
+    /// Class EmailHelpers.
+    /// </summary>
+    public static class EmailHelpers
+    {
+        /// <summary>
+        /// Creates the test email address based on Type FullName.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="localPart">The localPart. Local part is account name or user name of mail box.</param>
+        /// <param name="domain">The domain.</param>
+        /// <returns>System.String.</returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">Email length must be less than 256 characters.</exception>
+        /// <example>{localPart}+{type.FullName}+{name}@{domain}</example>
+        public static string Create(Type type, string name, string localPart = "test", string domain = "aurochses.com")
+        {
+            var email = $"{localPart}+{type.FullName}+{name}@{domain}";
+
+            if (email.Length > 256)
+            {
+                throw new ArgumentOutOfRangeException(nameof(email), email, "Email length must be less than 256 characters.");
+            }
+
+            return email;
+        }
+    }
+}
---
+++ test/Aurochses.Testing.Tests/ProjectHelpersTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.PlatformAbstractions;
+using Xunit;
+
+namespace Aurochses.Testing.Tests
+{
+    public class ProjectHelpersTests
+    {
+        private readonly string _applicationBasePath;
+
+        public ProjectHelpersTests()
+        {
+            _applicationBasePath = PlatformServices.Default.Application.ApplicationBasePath;
+        }
+
+        [Fact]
+        public void GetPath_Success()
+        {
+            // Arrange
+            const string solutionName = "Aurochses.Testing";
+
+            var path = new Regex($@"\S+\\{solutionName}\\").Match(_applicationBasePath).Value;
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                path = new Regex(@"\S+\\Aurochses.Testing.Tests").Match(_applicationBasePath).Value;
+                path = path.Replace(@"\test\Aurochses.Testing.Tests", string.Empty);
+            }
+
+            // Act & Assert
+            Assert.Equal(Path.Combine(path, "src", "Aurochses.Testing"), ProjectHelpers.GetPath(solutionName, "src", "Aurochses.Testing"));
+        }
+
+        [Fact]
+        public void GetPath_ThrowException_WhenSolutionNotFound()
+        {
+            // Arrange & Act & Assert
+            var exception = Assert.Throws<Exception>(() => ProjectHelpers.GetPath("Test", "src", "Test"));
+            Assert.Equal($"Solution root could not be located using application root {_applicationBasePath}.", exception.Message);
+        }
+    }
+}
